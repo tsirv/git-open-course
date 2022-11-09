4515477793c9274174c5c5335a85eb3109048920
tsirv <70756039+tsirv@users.noreply.github.com>
2022-11-09 14:24:44 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print('Hello World!')
-=======
-print('Hello world!')
->>>>>>> d1eccac2b2cca4066da67e85b93deb13d4d2e457
 
-=======
-print("Hello world!")
->>>>>>> upstream/master
+print('Hello World!')
+
